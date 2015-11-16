dc725c848a779a97e5dd176b2a07943878135293
Marcelo Almeida <marcelocralmeida@gmail.com>
2015-11-16 00:21:29 -0200

    Add feature to allow moving a tab to a new window

--- background.js
+++ background.js
@@ -1,51 +1,46 @@
 // Copyright (c) 2015 Marcelo Almeida. All rights reserved.
 
 function openSelectionOnGoogleMaps(info, tab) {
-    var url = "https://www.google.com/maps?q=";
-    var selectionText = info.selectionText;
+  var url = "https://www.google.com/maps?q=";
+  var selectionText = info.selectionText;
 
-    chrome.tabs.create({
-        url: url + selectionText
+  chrome.tabs.create({
+    url: url + selectionText
+  });
+
+  chrome.storage.sync.get(['history', 'historyItems'], function updateHistory(items) {
+    var saveHistory = items.history;
+    if (!saveHistory) {
+      return;
+    }
+
+    var historyItems = items.historyItems;
+    chrome.storage.sync.set({
+      historyItems: historyItems.concat(selectionText)
     });
-
-    chrome.storage.sync.get(['history', 'historyItems'], function updateHistory(items) {
-        var saveHistory = items.history;
-        if (!saveHistory) {
-            return;
-        }
-
-        var historyItems = items.historyItems;
-        chrome.storage.sync.set({
-            historyItems: historyItems.concat(selectionText)
-        });
-    })
+  })
 }
 
 function navigateToSelection(info, tab) {
-    var url = "https://www.google.com.br/maps/dir/rua paraibuna, sao jose dos campos/centervale";
+  var url = "https://www.google.com.br/maps/dir/rua paraibuna, sao jose dos campos/centervale";
 
 }
 
 // Only install context menus once.
 chrome.runtime.onInstalled.addListener(function() {
 
-    chrome.contextMenus.create({
-        title: "Open in Google Maps",
-        contexts: ["selection"],
-        onclick: openSelectionOnGoogleMaps
-    });
+  chrome.contextMenus.create({
+    title: "Open in Google Maps",
+    contexts: ["selection"],
+    onclick: openSelectionOnGoogleMaps
+  });
 
-    //var directionsParent = chrome.contextMenus.create({
-    //    "title": "Send to Maps",
-    //    "contexts": ["selection"],
-    //    "onclick": navigateToSelection
-    //});
+  //var directionsParent = chrome.contextMenus.create({
+  //    "title": "Send to Maps",
+  //    "contexts": ["selection"],
+  //    "onclick": navigateToSelection
+  //});
 
-    chrome.alarms.create('initialize-history-items', {when: Date.now()});
-});
-
-chrome.alarms.onAlarm.addListener(function (alarm) {
-    if (alarm.name === 'initialize-history-items') {
-        initializeHistoryItems();
-    }
+  chrome.alarms.create('initialize-history-items', {when: Date.now()});
+  chrome.storage.local.set({tabsIds: {}});
 });
