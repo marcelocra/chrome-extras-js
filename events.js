4dd61f25561239af9eaf3e49a4370f1f519d67c9
Marcelo Almeida <marcelocralmeida@gmail.com>
2015-11-18 23:17:00 -0200

    Finish javascript version

--- events.js
+++ events.js
@@ -1,6 +1,6 @@
 var events = {
-  INITIALIZE_HISTORY_ITEMS: 'initialize-history-items',
-  MOVE_TAB_TO_WINDOW: 'move-tab-to-window',
+    INITIALIZE_HISTORY_ITEMS: 'initialize-history-items',
+    MOVE_TAB_TO_WINDOW: 'move-tab-to-window',
 };
 
 // Data structure used to hold all constants that must be shared between
@@ -9,14 +9,17 @@ chrome.storage.local.set({events: events});
 
 // Register all alarms that the system must listen to.
 chrome.alarms.onAlarm.addListener(function (alarm) {
-  if (alarm.name === events.INITIALIZE_HISTORY_ITEMS) {
-    initializeHistoryItems();
-  }
+    if (alarm.name === events.INITIALIZE_HISTORY_ITEMS) {
+        initializeHistoryItems();
+    }
 });
 
-
 chrome.commands.onCommand.addListener(function (command) {
-  if (command === events.MOVE_TAB_TO_WINDOW) {
-    toggleTabToWindow();
-  }
+    if (command === events.MOVE_TAB_TO_WINDOW) {
+        toggleTabToWindow();
+    }
+});
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    openOrFocusOptionsPage();
 });
